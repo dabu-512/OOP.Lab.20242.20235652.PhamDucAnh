ceb251160238bdb6974c96de97e24039f401d0c0
Pham Duc Anh <anh.pd235652@sis.hust.edu.vn>
2025-03-02 18:19:10 +0700

    add lab01

--- Lab01/Ex2_2_5.java
+++ Lab01/Ex2_2_5.java
@@ -1,31 +1,34 @@
-
 import javax.swing.JOptionPane;
 
-public class Ex2_2_5{
+public class Ex2_2_5 {
     public static void main(String[] args) {
+
         String strNum1 = JOptionPane.showInputDialog(null, 
-                "Please input the first number:", "Input", JOptionPane.INFORMATION_MESSAGE);
+                "Nhập số thứ nhất:", "Input", JOptionPane.INFORMATION_MESSAGE);
         String strNum2 = JOptionPane.showInputDialog(null, 
-                "Please input the second number:", "Input", JOptionPane.INFORMATION_MESSAGE);
+                "Nhập số thứ hai:", "Input", JOptionPane.INFORMATION_MESSAGE);
 
         double num1 = Double.parseDouble(strNum1);
         double num2 = Double.parseDouble(strNum2);
+
         double tong = num1 + num2;
         double hieu = num1 - num2;
         double tich = num1 * num2;
-	    double thuong = 0;
-	    if (num2 != 0) {
-		    thuong = num1 / num2;
-	    }
-
-        System.out.println("Tong hai so la: " + tong);
-        System.out.println("Hieu hai so la: " + hieu);
-        System.out.println("Tich hai so la: " + tich);
+        String thuong;
+        
         if (num2 != 0) {
-            System.out.println("Thuong hai so la: " + thuong);
-        } 
-	    else {
-            System.out.println("Khong thuc hien duoc phep chia");
+            thuong = String.valueOf(num1 / num2);
+        } else {
+            thuong = "Không thể thực hiện phép chia";
         }
+
+        String result = "Tổng: " + tong + "\n" +
+                        "Hiệu: " + hieu + "\n" +
+                        "Tích: " + tich + "\n" +
+                        "Thương: " + thuong;
+
+        JOptionPane.showMessageDialog(null, result, "Calculation Result", JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
     }
 }
