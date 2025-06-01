a816d336533f51284ea7976554d31f54645e83f8
dabu-512 <anh.pd235652@sis.hust.edu.vn>
2025-06-02 01:58:02 +0700

    update test

--- Lab05/AIMSproject/src/hust/soict/hedspi/aims/test/controller/TestViewStoreScreen.java
+++ Lab05/AIMSproject/src/hust/soict/hedspi/aims/test/controller/TestViewStoreScreen.java
@@ -34,17 +34,35 @@ public class TestViewStoreScreen extends Application {
     public static void main(String[] args) {
     	store = new Store();
         cart = new Cart();
-        store.addMedia(new DigitalVideoDisc(1,"The Lion King", "Animation", 19.95f, "Roger Allers", 87));
-        store.addMedia(new CompactDisc(2 , "Hits of 90s", "Music", 15.5f, "Various Artists",30, "Best Seller"));
-        store.addMedia(new CompactDisc(3, "Journey With You", "Music", 15.50f, "Queen", 60, "Iris"));
-        store.addMedia(new Book(4, "Design Patterns", "Programming", 35.0f));
-        store.addMedia(new DigitalVideoDisc(5, "Star Wars", "Science Fiction", 24.95f, "George Lucas", 124));
-        store.addMedia(new Book(6, "C Language", "Programming", 45.00f));
-        store.addMedia(new CompactDisc(7, "Lo-fi Chill", "Instrumental", 10.99f, "Various", 50, "LoChill"));
-        store.addMedia(new CompactDisc(8, "Rock Legends", "Rock", 18.0f, "RockStar", 70, "Thunder Band"));
-        store.addMedia(new Book(9, "Design Patterns", "Programming", 35.0f));
-        store.addMedia(new Book(10, "The Pragmatic Programmer", "Software", 38.5f));
-
+        store.addMedia(new DigitalVideoDisc("The Lion King", "Animation", 19.95f, "Roger Allers", 87));
+        store.addMedia(new CompactDisc("Hits of 90s", "Music", 15.5f, "Various Artists",30, "Best Seller"));
+        store.addMedia(new CompactDisc("Journey With You", "Music", 15.50f, "Queen", 60, "Iris"));
+        store.addMedia(new Book("Design Patterns", "Programming", 35.0f));
+        store.addMedia(new DigitalVideoDisc("Star Wars", "Science Fiction", 24.95f, "George Lucas", 124));
+        store.addMedia(new Book("C Language", "Programming", 45.00f));
+        store.addMedia(new CompactDisc("Lo-fi Chill", "Instrumental", 10.99f, "Various", 50, "LoChill"));
+        store.addMedia(new CompactDisc("Rock Legends", "Rock", 18.0f, "RockStar", 70, "Thunder Band"));
+        store.addMedia(new Book("Design Patterns", "Programming", 35.0f));
+        store.addMedia(new Book("The Pragmatic Programmer", "Software", 38.5f));
+        store.addMedia(new DigitalVideoDisc("Interstellar", "Sci-Fi", 25.5f, "Christopher Nolan", 169));
+        store.addMedia(new DigitalVideoDisc("Gladiator", "Historical", 21.0f, "Ridley Scott", 155));
+        store.addMedia(new DigitalVideoDisc("Shrek", "Animation", 17.0f, "Andrew Adamson", 90));
+        store.addMedia(new DigitalVideoDisc("Joker", "Drama", 23.5f, "Todd Phillips", 122));
+        store.addMedia(new CompactDisc("Jazz Essentials", "Jazz", 16.5f, "Jazz Band", 65, "Smooth Flow"));
+        store.addMedia(new CompactDisc("Classical Moods", "Classical", 13.75f, "Various", 75, "Orchestra"));
+        store.addMedia(new CompactDisc("EDM Beats", "EDM", 19.25f, "DJ Max", 45, "Pulse"));
+        store.addMedia(new CompactDisc("Acoustic Nights", "Acoustic", 12.5f, "Guitar Hero", 55, "Nightfall"));
+        store.addMedia(new CompactDisc("Vietnamese Hits", "V-Pop", 11.5f, "Various", 60, "Top V Hits"));
+        store.addMedia(new Book("Refactoring", "Software Engineering", 42.0f));
+        store.addMedia(new Book("Effective Java", "Programming", 37.5f));
+        store.addMedia(new Book("Introduction to Algorithms", "Computer Science", 50.0f));
+        store.addMedia(new Book("Head First Design Patterns", "Programming", 36.0f));
+        store.addMedia(new Book("Domain-Driven Design", "Architecture", 46.0f));
+        store.addMedia(new Book("The Art of Computer Programming", "CS Theory", 55.0f));
+        CompactDisc brokenCD = new CompactDisc("Silent CD", "Music", 12.5f, "Ghost Artist", 40, "Ghost Band");
+        store.addMedia(brokenCD);
+        DigitalVideoDisc brokenDVD = new DigitalVideoDisc("Broken DVD", "Error ", 9.99f, "No Director", 0);
+        store.addMedia(brokenDVD);
         launch(args);
     }
 }
